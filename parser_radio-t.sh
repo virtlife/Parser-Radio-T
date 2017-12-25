5f8a980b9a67e1573cdff8bfc8cdcafcb73a4250
Banana Pi Server <bpisrv@gmail.com>
2017-12-25 20:31:02 +0600

    Add parser_radio-t.sh

---
+++ parser_radio-t.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Parser of weekly podcasts.
+
+DOWNLOAD=`curl -s https://radio-t.com | grep "http://cdn.radio-t.com/" | grep -o "http[^>]*.mp3" | sed 'n;d' | sed '2,$d'`
+
+#curl -s https://radio-t.com | grep "http://cdn.radio-t.com/" | awk -F\" '{print $2}' | head -n1
+#Влад сказал учи awk и head
+
+wget -c -P /mnt16/radio-t/ $DOWNLOAD
+
+# -c докачивание в случае обрыва
+# -P путь куда именно скачать
+
+echo -e '\e[32m'"Done!"
+
